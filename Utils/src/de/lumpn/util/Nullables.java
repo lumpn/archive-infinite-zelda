7f1170cedd2f5fc0158538a655b7afe0cb0a5674
lumpn <lumpn@users.noreply.github.com>
2014-05-20 14:46:20 -0700

    Simplified nullable helpers.

--- Utils/src/de/lumpn/util/Nullables.java
+++ Utils/src/de/lumpn/util/Nullables.java
@@ -2,13 +2,11 @@ package de.lumpn.util;
 
 public final class Nullables {
 
-	public static <T> int hashCode(T obj) {
-		if (obj == null) return 0;
-		return obj.hashCode();
+	public static <T> boolean equals(T a, T b) {
+		return (a == null) ? (b == null) : a.equals(b);
 	}
 
-	public static <T> boolean equals(T a, T b) {
-		if (a == null) return (b == null);
-		return a.equals(b);
+	public static <T> int hashCode(T obj) {
+		return (obj == null) ? 0 : obj.hashCode();
 	}
 }
