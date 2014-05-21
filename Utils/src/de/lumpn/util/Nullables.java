24eeff0161e3fc0e0fba3a13c6c85fec93561e38
lumpn <lumpn@users.noreply.github.com>
2014-05-20 22:43:21 -0700

    Improved formatting.

--- Utils/src/de/lumpn/util/Nullables.java
+++ Utils/src/de/lumpn/util/Nullables.java
@@ -2,11 +2,11 @@ package de.lumpn.util;
 
 public final class Nullables {
 
-	public static <T> boolean equals(T a, T b) {
-		return (a == null) ? (b == null) : a.equals(b);
-	}
-
 	public static <T> int hashCode(T obj) {
 		return (obj == null) ? 0 : obj.hashCode();
+	}
+	
+	public static <T> boolean equals(T a, T b) {
+		return (a == null) ? (b == null) : a.equals(b);
 	}
 }
