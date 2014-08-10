98fa05332c172f3a741c3419bcaa35279fddcde5
lumpn <lumpn@users.noreply.github.com>
2014-08-10 17:06:54 +1000

    Missed file.

---
+++ Utils/src/de/lumpn/report/MockProgressBar.java
@@ -0,0 +1,12 @@
+package de.lumpn.report;
+
+public class MockProgressBar implements ProgressConsumer {
+
+	@Override
+	public void reset(String caption) {
+	}
+
+	@Override
+	public void set(int current, int total) {
+	}
+}
